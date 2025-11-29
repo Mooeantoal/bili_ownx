3d87a6d076066620ed4b112d0369a94de3511701
Mooeantoal <sy878614006@gmail.com>
2025-11-29 20:30:37 +0800

    6

--- android/app/src/main/kotlin/com/example/bili_ownx/FlutterApplication.kt
+++ android/app/src/main/kotlin/com/example/bili_ownx/FlutterApplication.kt
@@ -1,15 +1,9 @@
 package com.example.bili_ownx
 
 import io.flutter.app.FlutterApplication
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class FlutterApplication : FlutterApplication() {
     override fun onCreate() {
         super.onCreate()
-    }
-
-    override fun registerFlutterEngine(flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 }
