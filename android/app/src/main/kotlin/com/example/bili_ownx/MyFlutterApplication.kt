134059b92c6974c438ac09ab351e036afa4645be
Mooeantoal <sy878614006@gmail.com>
2025-11-30 19:36:45 +0800

    feat(android): 添加自定义 FlutterApplication 类
    - 创建 MyFlutterApplication 类，继承自 FlutterApplication
    - 重写 onCreate 方法，为后续的全局初始化操作提供入口

---
+++ android/app/src/main/kotlin/com/example/bili_ownx/MyFlutterApplication.kt
@@ -0,0 +1,9 @@
+package com.example.bili_ownx
+
+import io.flutter.app.FlutterApplication
+
+class MyFlutterApplication : FlutterApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
