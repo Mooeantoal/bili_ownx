f44c42b359f29e44ce7f35636223a4616580e3c4
Mooeantoal <sy878614006@gmail.com>
2025-11-30 17:39:54 +0800

    11

--- android/app/src/main/kotlin/com/example/bili_ownx/FlutterApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bili_ownx
-
-import io.flutter.embedding.android.FlutterApplication
-
-class MyFlutterApplication : FlutterApplication() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
