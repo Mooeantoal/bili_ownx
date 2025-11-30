e4e3dc98078c2cb45ece94b4acb5e07224bd93f9
Mooeantoal <sy878614006@gmail.com>
2025-11-30 20:09:28 +0800

    refactor(android): 移除未使用的 MyFlutterApplication 类
    - 从 AndroidManifest.xml 中移除了对 MyFlutterApplication 的引用
    - 删除了 MyFlutterApplication.kt 文件，该文件定义了一个空的 FlutterApplication 子类
    - 这些更改有助于简化项目结构并减少冗余代码

--- android/app/src/main/kotlin/com/example/bili_ownx/MyFlutterApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bili_ownx
-
-import io.flutter.app.FlutterApplication
-
-class MyFlutterApplication : FlutterApplication() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
