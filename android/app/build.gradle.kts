2eb6316995794b6e11b18fd2f942696309077966
Mooeantoal <sy878614006@gmail.com>
2025-12-05 23:10:15 +0800

    ci: 更新 Flutter 和 Android SDK 设置
    - 将 subosito/flutter-action 版本从 v2 升级到 v3
    - 使用 android-actions/setup-android 设置 Android SDK
    - 修复 build.gradle.kts 中的 minSdkVersion 语法错误，统一使用 minSdkVersion(21) 格式
    - 更新 Java 版本为 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "com.example.bili_ownx"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdkVersion = 21
+        minSdkVersion(21)
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
