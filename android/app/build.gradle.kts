ccc1fc005ad2c3e76dadb4490025dcbbfd6da935
Mooeantoal <sy878614006@gmail.com>
2025-12-10 15:50:42 +0800

    fix(build): 将Android SDK版本从35降级到34，更新相关构建配置

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.bili_ownx"
-    compileSdk = 35
+    compileSdk = 34
     ndkVersion = "27.0.12077973"
     
     // 启用核心库脱糖以支持 Java 8+ 特性
@@ -35,7 +35,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
